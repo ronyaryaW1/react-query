eafb858c81231294d16c7422602f7d3b707264d4
Rony Arya Wijaya <rony.wijaya@ccn.com.sg>
2025-03-23 05:16:08 +0700

    Add React Query integration and configure image remote patterns

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+	/* config options here */
+	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'fakestoreapi.com',
+				port: '',
+				pathname: '/**',
+				search: '',
+			},
+		],
+	},
 };
 
 export default nextConfig;
