d49b83f30e0bf24cff38ad38f588e9316b1efb07
Rony Arya Wijaya <rony.wijaya@ccn.com.sg>
2025-03-23 05:45:19 +0700

    query: Enhance product loading state handling with error and loading indicators

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,18 +11,44 @@ interface Product {
 }
 
 export default function Home() {
-	const query = useQuery({
+	const { isLoading, data, isError } = useQuery({
 		queryKey: ['products'],
 		queryFn: async () => {
 			const res = await fetch('https://fakestoreapi.com/products');
 			return res.json();
 		},
+		enabled: true, // by default it's true this is for handling the query to be called or not
 	});
 
+	if (isError) {
+		return (
+			<div>
+				<h1>something went wrong</h1>
+			</div>
+		);
+	}
+
+	if (isLoading) {
+		return (
+			<div className='grid grid-cols-1 gap-4 lg:grid-cols-5 p-8'>
+				{Array.from({ length: 10 }).map((_, index) => {
+					return (
+						<div
+							key={index}
+							className='bg-white p-4 shadow rounded flex flex-col items-center justify-center animate-pulse aspect-square'
+						>
+							<div className='bg-gray-200 h-40 w-40' />
+							<div className='bg-gray-200 h-4 w-20 mt-2' />
+							<div className='bg-gray-200 h-4 w-20 mt-2' />
+						</div>
+					);
+				})}
+			</div>
+		);
+	}
 	return (
-		<div className='container mx-auto grid grid-cols-1 gap-4 lg:grid-cols-5'>
-			{query.isLoading && <p>Loading...</p>}
-			{query?.data?.map((product: Product) => {
+		<div className='container mx-auto grid grid-cols-1 gap-4 lg:grid-cols-5 p-8'>
+			{data?.map((product: Product) => {
 				return (
 					<div
 						key={product.id}
